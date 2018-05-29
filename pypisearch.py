450d33c2956212e766e9ada5a92c90399c6f30d1
ghostinterminal <39333404+ghostinterminal@users.noreply.github.com>
2018-05-29 16:33:11 +0530

    Add files via upload

---
+++ pypisearch.py
@@ -0,0 +1,33 @@
+import mechanize
+from bs4 import BeautifulSoup
+import requests
+
+class Pypi:
+    def search(self):
+        url = 'https://pypi.org/search/?q='
+        search = raw_input('Name of Module: ')
+        url += search
+        data = requests.get(url).text
+        soup = BeautifulSoup(data, 'lxml')
+        h3s = soup.findAll('h3', class_='package-snippet__title')
+        new_data = str(h3s)
+        new_soup = BeautifulSoup(new_data, 'lxml')
+        links = new_soup.find_all('a')
+        for link in links:
+            print ''
+            print 'Module Name:',link.text
+            link = 'https://pypi.org'+str(link['href'])
+            print 'Link: ',link
+            command = self.command(link)
+            print 'Command: '+str(command)
+    def command(self, url):
+        data = requests.get(url).text
+        soup = BeautifulSoup(data, 'lxml')
+        spans = soup.findAll('span', id='pip-command')
+        for spam in spans:
+            return spam.text
+
+
+
+h = Pypi()
+h.search()
