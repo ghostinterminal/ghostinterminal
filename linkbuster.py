8d1c256fcf79985111b5d11b645343deed87052d
ghostinterminal <39333404+ghostinterminal@users.noreply.github.com>
2018-08-16 20:47:00 +0530

    Add files via upload

--- linkbuster.py
+++ linkbuster.py
@@ -1,13 +1,15 @@
-import mechanize
+import requests
 from bs4 import BeautifulSoup
 import sys
 
 url = str(sys.argv[1])
-br = mechanize.Browser()
-br.set_handle_robots(False)
-data = br.open(url).read()
+br = requests.get(url, headers={'User-agent':'Mozzila/5.0'})
+data = br.text
 soup = BeautifulSoup(data, 'lxml')
 links = soup.find_all('a')
+paths = soup.find_all('img')
+paths += soup.find_all('script')
+print '====================== LINKS FOUNDED ===========================\n'
 for link in links:
     try:
     	l = str(link['href'])
@@ -17,3 +19,19 @@ for link in links:
         	print l
     except:
 	  print 'Error'
+print '====================== END ======================================'
+
+print '\n=================== Directoires Found =========================\n'
+for path in paths:
+	try:
+		if not path['src']:
+			pass
+		else:
+			p = str(path['src'])
+			print  p
+	except Exception as e:
+		pass
+
+print '==================== END ========================================='
+
+		
